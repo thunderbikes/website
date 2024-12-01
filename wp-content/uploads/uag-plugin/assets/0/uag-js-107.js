b09d3e9f337ff1a4b48d9bd016f2de8eb737b5a1
Andreas <andreasboscariol@gmail.com>
2024-11-30 17:09:26 -0800

    website update

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-107.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function(){ jQuery(document).ready(function () {
+				let scope = jQuery(".wp-block-uagb-image-gallery.uagb-block-66c5d3be");
+				if ( scope.length ) { 
+					scope.css("visibility", "visible");
+					let getSlickCarousel = scope.find(".uagb-slick-carousel");
+					if( getSlickCarousel.length ) {
+						getSlickCarousel.slick({"arrows":true,"dots":true,"initialSlide":0,"infinite":true,"autoplay":true,"autoplaySpeed":5000,"pauseOnHover":true,"speed":500,"slidesToShow":1,"prevArrow":"<button type='button' data-role='none' class='spectra-image-gallery__control-arrows spectra-image-gallery__control-arrows--carousel slick-prev slick-arrow' aria-label='Previous' tabindex='0' role='button'><svg xmlns='http:\/\/www.w3.org\/2000\/svg' viewBox='0 0 256 512' width='26' height='26'><path d='M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z'><\/path><\/svg><\/button>","nextArrow":"<button type='button' data-role='none' class='spectra-image-gallery__control-arrows spectra-image-gallery__control-arrows--carousel slick-next slick-arrow' aria-label='Previous' tabindex='0' role='button'><svg xmlns='http:\/\/www.w3.org\/2000\/svg' viewBox='0 0 256 512' width='26' height='26'><path d='M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z'><\/path><\/svg><\/button>","rtl":false,"responsive":[{"breakpoint":1024,"settings":{"slidesToShow":1}},{"breakpoint":767,"settings":{"slidesToShow":1}}]});
+					}
+				}
+			});jQuery(document).ready(function () {
+				let scope = jQuery(".wp-block-uagb-image-gallery.uagb-block-3f40cf66");
+				if ( scope.length ) { 
+					scope.css("visibility", "visible");
+					let getSlickCarousel = scope.find(".uagb-slick-carousel");
+					if( getSlickCarousel.length ) {
+						getSlickCarousel.slick({"arrows":true,"dots":true,"initialSlide":0,"infinite":true,"autoplay":true,"autoplaySpeed":5000,"pauseOnHover":true,"speed":500,"slidesToShow":1,"prevArrow":"<button type='button' data-role='none' class='spectra-image-gallery__control-arrows spectra-image-gallery__control-arrows--carousel slick-prev slick-arrow' aria-label='Previous' tabindex='0' role='button'><svg xmlns='http:\/\/www.w3.org\/2000\/svg' viewBox='0 0 256 512' width='26' height='26'><path d='M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z'><\/path><\/svg><\/button>","nextArrow":"<button type='button' data-role='none' class='spectra-image-gallery__control-arrows spectra-image-gallery__control-arrows--carousel slick-next slick-arrow' aria-label='Previous' tabindex='0' role='button'><svg xmlns='http:\/\/www.w3.org\/2000\/svg' viewBox='0 0 256 512' width='26' height='26'><path d='M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z'><\/path><\/svg><\/button>","rtl":false,"responsive":[{"breakpoint":1024,"settings":{"slidesToShow":1}},{"breakpoint":767,"settings":{"slidesToShow":1}}]});
+					}
+				}
+			});jQuery(document).ready(function () {
+				let scope = jQuery(".wp-block-uagb-image-gallery.uagb-block-dc198889");
+				if ( scope.length ) { 
+					scope.css("visibility", "visible");
+					let getSlickCarousel = scope.find(".uagb-slick-carousel");
+					if( getSlickCarousel.length ) {
+						getSlickCarousel.slick({"arrows":true,"dots":true,"initialSlide":0,"infinite":true,"autoplay":true,"autoplaySpeed":5000,"pauseOnHover":true,"speed":500,"slidesToShow":1,"prevArrow":"<button type='button' data-role='none' class='spectra-image-gallery__control-arrows spectra-image-gallery__control-arrows--carousel slick-prev slick-arrow' aria-label='Previous' tabindex='0' role='button'><svg xmlns='http:\/\/www.w3.org\/2000\/svg' viewBox='0 0 256 512' width='24' height='24'><path d='M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z'><\/path><\/svg><\/button>","nextArrow":"<button type='button' data-role='none' class='spectra-image-gallery__control-arrows spectra-image-gallery__control-arrows--carousel slick-next slick-arrow' aria-label='Previous' tabindex='0' role='button'><svg xmlns='http:\/\/www.w3.org\/2000\/svg' viewBox='0 0 256 512' width='24' height='24'><path d='M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z'><\/path><\/svg><\/button>","rtl":false,"responsive":[{"breakpoint":1024,"settings":{"slidesToShow":1}},{"breakpoint":767,"settings":{"slidesToShow":1}}]});
+					}
+				}
+			});jQuery(document).ready(function () {
+				let scope = jQuery(".wp-block-uagb-image-gallery.uagb-block-3f2d03b7");
+				if ( scope.length ) { 
+					scope.css("visibility", "visible");
+					let getSlickCarousel = scope.find(".uagb-slick-carousel");
+					if( getSlickCarousel.length ) {
+						getSlickCarousel.slick({"arrows":true,"dots":true,"initialSlide":0,"infinite":true,"autoplay":true,"autoplaySpeed":5000,"pauseOnHover":true,"speed":500,"slidesToShow":1,"prevArrow":"<button type='button' data-role='none' class='spectra-image-gallery__control-arrows spectra-image-gallery__control-arrows--carousel slick-prev slick-arrow' aria-label='Previous' tabindex='0' role='button'><svg xmlns='http:\/\/www.w3.org\/2000\/svg' viewBox='0 0 256 512' width='24' height='24'><path d='M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z'><\/path><\/svg><\/button>","nextArrow":"<button type='button' data-role='none' class='spectra-image-gallery__control-arrows spectra-image-gallery__control-arrows--carousel slick-next slick-arrow' aria-label='Previous' tabindex='0' role='button'><svg xmlns='http:\/\/www.w3.org\/2000\/svg' viewBox='0 0 256 512' width='24' height='24'><path d='M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z'><\/path><\/svg><\/button>","rtl":false,"responsive":[{"breakpoint":1024,"settings":{"slidesToShow":1}},{"breakpoint":767,"settings":{"slidesToShow":1}}]});
+					}
+				}
+			});jQuery(document).ready(function () {
+				let scope = jQuery(".wp-block-uagb-image-gallery.uagb-block-790f87be");
+				if ( scope.length ) { 
+					scope.css("visibility", "visible");
+					let getSlickCarousel = scope.find(".uagb-slick-carousel");
+					if( getSlickCarousel.length ) {
+						getSlickCarousel.slick({"arrows":true,"dots":true,"initialSlide":0,"infinite":true,"autoplay":true,"autoplaySpeed":5000,"pauseOnHover":true,"speed":500,"slidesToShow":1,"prevArrow":"<button type='button' data-role='none' class='spectra-image-gallery__control-arrows spectra-image-gallery__control-arrows--carousel slick-prev slick-arrow' aria-label='Previous' tabindex='0' role='button'><svg xmlns='http:\/\/www.w3.org\/2000\/svg' viewBox='0 0 256 512' width='24' height='24'><path d='M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z'><\/path><\/svg><\/button>","nextArrow":"<button type='button' data-role='none' class='spectra-image-gallery__control-arrows spectra-image-gallery__control-arrows--carousel slick-next slick-arrow' aria-label='Previous' tabindex='0' role='button'><svg xmlns='http:\/\/www.w3.org\/2000\/svg' viewBox='0 0 256 512' width='24' height='24'><path d='M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z'><\/path><\/svg><\/button>","rtl":false,"responsive":[{"breakpoint":1024,"settings":{"slidesToShow":1}},{"breakpoint":767,"settings":{"slidesToShow":1}}]});
+					}
+				}
+			}); });
